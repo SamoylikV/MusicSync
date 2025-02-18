bb348d4ad3cf9390f1191f48c4165a16e5390784
Serdgio1 <suprun_777@list.ru>
2025-02-18 21:02:45 +0300

    refactor

--- yandex.py
+++ yandex.py
@@ -2,15 +2,50 @@ from yandex_music import Client
 from config import YAN_TOKEN
 from rediska import r
 
-client = Client(YAN_TOKEN).init()
-tracks = {}
-counter = 0
-for i in client.users_likes_tracks().fetch_tracks():
-    if i['type']=='music':
-#        tracks[i['title']] = i['artists'][0]['name']
+class Yandex:
+    def __init__(self,Token):
+        self.token = Token
+        self.client = Client(Token).init()
+    
+    def yan_to_red(self, r):
         
-        r.set(f'yandex:{counter}:title',i['title'] )
-        r.set(f'yandex:{counter}:artist',i['artists'][0]['name'] )
-        counter+=1
+        counter = 0
+        for i in self.client.users_likes_tracks().fetch_tracks():
+            if i['type']=='music':
+        #        tracks[i['title']] = i['artists'][0]['name']
+                r.set(f'yandex:{counter}:title',i['title'] )
+                r.set(f'yandex:{counter}:artist',i['artists'][0]['name'] )
+                counter+=1
+        return r.ping()
+    
+    def likes(self):
+        while True:
+            counter = 0
+            if r.get(f'spotify:{counter}:title') == None:
+                break
+            title = r.get(f'spotify:{counter}:title')
+            artist = r.get(f'spotify:{counter}:artist')
+            search_res = self.client.search(f'{artist} {title}')
+            if search_res.tracks != None:
+                track_id = search_res.tracks['results'][0]['id']
+                self.client.users_likes_tracks_add(track_id)
+        return r.ping()
+        
 
-        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
